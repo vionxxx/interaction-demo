b1bda8cc4c7122bac36cc1b095a0a6244d7f971f
vionxxx <Abbsinthev@gmail.com>
2021-10-06 23:48:24 +0800

    try

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,31 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDatlkjoNNHIFCEGbo8rpz6dkwa8_IrNYc",
+    authDomain: "interaction-fcc80.firebaseapp.com",
+    projectId: "interaction-fcc80",
+    storageBucket: "interaction-fcc80.appspot.com",
+    messagingSenderId: "556736747139",
+    appId: "1:556736747139:web:8229f7699f96f0d67eadb6",
+    measurementId: "G-DSQP9WWXKV"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
